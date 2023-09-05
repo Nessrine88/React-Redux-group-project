bcad13debae2b38d48ee9c89f472c915a723dbcd
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-09-04 20:33:43 -0500

    Add initial set up V-six

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger'; 
-
-
+import { legacy_createStore as createStore} from 'redux'
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 
-
 const middlewares = [logger];
-
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
