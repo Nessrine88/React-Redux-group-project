847d3f1165cce1903e7ce1927892e46673d6ded5
unknown <macherki.nessrine@yahoo.com>
2023-09-06 15:11:11 +0100

    add mission table from fteched api data

--- src/components/Mission/Mission.js
+++ src/components/Mission/Mission.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMissions } from '../../redux/Mission/MissionSlice';
+import './Mission.css';
 
 function Mission() {
   const missions = useSelector((state) => state.missions.missions);
@@ -12,15 +13,23 @@ function Mission() {
 
   return (
     <div>
-      <h2>My mission page</h2>
-      <ul>
-        {missions.map((mission) => (
-          <li key={mission.mission_id}>
-            <h3>{mission.mission_name}</h3>
-            <p>{mission.description}</p>
-          </li>
+      <table className="MissionTable">
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>{' '}</th>
+
+        </tr>
+        {missions.map((mission, index) => (
+          <tr key={mission.mission_id} className={index % 2 === 0 ? 'gray-background' : 'white-background'}>
+            <td className="MissionName">{mission.mission_name}</td>
+            <td className="MissionDescription">{mission.description}</td>
+            <td className="member"><button type="button">Not A Member</button></td>
+            <td className="join"><button type="button">Join Mission</button></td>
+          </tr>
         ))}
-      </ul>
+      </table>
     </div>
   );
 }
--- src/redux/Mission/MissionSlice.js
+++ src/redux/Mission/MissionSlice.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 const baseUrl = 'https://api.spacexdata.com/v3/missions';
 
-// Async thunk to fetch missions
 export const fetchMissions = createAsyncThunk('missions/fetchMissions', async () => {
   try {
     const response = await axios.get(baseUrl);
@@ -13,25 +12,20 @@ export const fetchMissions = createAsyncThunk('missions/fetchMissions', async ()
   }
 });
 
-// Initial state for missions
 const initialState = {
-  missions: [], // You want to store missions in an array
+  missions: [],
 };
 
-// Create a mission slice
 const missionSlice = createSlice({
-  name: 'missions', // This should be 'missions' instead of 'Mission'
+  name: 'missions',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchMissions.fulfilled, (state, action) => {
-        state.missions = action.payload; // Update the missions array with the fetched data
-      })
-      .addCase(fetchMissions.rejected, () => {
-        // Handle the error state if needed
+        state.missions = action.payload;
       });
   },
 });
 
-export default missionSlice.reducer; // Export the reducer
+export default missionSlice.reducer;
