3bdccead5d3390dcfc23d26e8d0da91c5f1d417b
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-09-06 16:08:57 -0500

    Add rocket setup

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import './Rockets.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRockets, selectRockets } from '../../redux/rockets/rocketsSlice';
 
 function Rockets() {
-  return (
-    <div>
-      <h1>This is my rocket file</h1>
-    </div>
-  );
+  const rockets = useSelector(selectRockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  let content;
+
+  if (rockets.status === 'loading') {
+    content = <div>Loading...</div>;
+  } else if (rockets.status === 'succeeded') {
+    content = (
+      <div>
+        {rockets.rockets.map((rocket) => (
+          <div key={rocket.id}>
+            <h2>{rocket.rocket_name}</h2>
+            <p>{rocket.description}</p>
+            <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+          </div>
+        ))}
+      </div>
+    );
+  } else if (rockets.status === 'failed') {
+    content = (
+      <div>
+        Error:
+        {rockets.error}
+      </div>
+    );
+  }
+
+  return <div>{content}</div>;
 }
 
 export default Rockets;
--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,26 +1,43 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchRockets } from '../../redux/rockets/rocketsSlice';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const Rockets = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchRockets());
-  }, [dispatch]);
+const baseUrl = 'https://api.spacexdata.com/v4/rockets';
 
-  const rockets = useSelector((state) => state.rockets);
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
+  try {
+    const response = await axios.get(baseUrl);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch rockets');
+  }
+});
 
-  return (
-    <div>
-      {rockets.map((rocket) => (
-        <div key={rocket.id}>
-          <h2>{rocket.rocket_name}</h2>
-          <p>{rocket.description}</p>
-          <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
-        </div>
-      ))}
-    </div>
-  );
+const initialState = {
+  rockets: [],
+  status: 'idle',
+  error: null,
 };
 
-export default Rockets;
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRockets.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.rockets = action.payload;
+      })
+      .addCase(fetchRockets.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectRockets = (state) => state.rockets; // Exporta selectRockets
+
+export default rocketsSlice.reducer;
